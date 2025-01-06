4c4ce8fd8eb2702c5fe3334b9a18e9f9549ada6b
lisa erksn <lisa.eriksen@epitech.eu>
2025-01-06 10:48:53 +0100

    first commit

---
+++ components/Library.tsx
@@ -0,0 +1,81 @@
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
+
+interface LibraryProps{
+  songs: Song[]
+}
+
+const Library: React.FC<LibraryProps> = ({songs}) => {
+  const authModal = useAuthModal()
+  const upLoadModal = useUploadModal()
+  const {user} = useUser()   //from hooks
+
+  const onClick = () => {
+
+    if(!user){
+      return authModal.onOpen();
+    }
+
+    return upLoadModal.onOpen();
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div
+        className="flex
+            items-center
+            justify-between
+            px-5
+            pt-4"
+      >
+        <div
+          className="
+            inline-flex
+            items-center
+            gap-x-2
+            "
+        >
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p
+            className="
+            text-neutral-400
+            font-medium
+            text-md"
+          >
+            Yout Library
+          </p>
+        </div>
+        <AiOutlinePlus 
+            onClick={onClick}
+            size={20}
+            className="
+            text-neutral-400
+            cursor-pointer
+            hover:text-white
+            transition
+            "
+            />
+      </div>
+      <div className="
+      flex
+      flex-col
+      gap-y-2
+      mt-4
+      px-3">
+        {songs.map((item)=>(
+          <MediaItem
+          key={item.id}
+          onClick={()=> {}}
+          data={item}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;

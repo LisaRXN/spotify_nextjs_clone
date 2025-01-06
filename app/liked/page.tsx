4c254361c1f88c497beadd7ba397ba38e61cd498
lisa erksn <lisa.eriksen@epitech.eu>
2025-01-06 11:14:36 +0100

    add liked songs

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,6 +1,7 @@
 import getLikedSongs from "@/actions/getLikedSongs"
 import Header from "@/components/Header"
 import Image from "next/image"
+import LikedContent from "./LikedContent"
 
 export const revalidate = 0
 
@@ -54,6 +55,7 @@ const Liked = async () => {
                     </div>
                 </div>
             </Header>
+            <LikedContent songs={songs}/>
         </div>
     )
 }
